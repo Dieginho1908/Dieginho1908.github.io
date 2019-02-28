9329a37138f7a54f4f7e54914dba90f4fbf78704
Diego Gonzalez <dieginho1908@gmail.com>
2019-02-28 13:28:27 -0600

    primer commit

---
+++ precache-manifest.2a0c06e171998ed3a71d8f1486451f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e303b0f2e962bac127ad",
+    "url": "/static/js/main.e303b0f2.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e303b0f2e962bac127ad",
+    "url": "/static/css/main.b1d90b9b.chunk.css"
+  },
+  {
+    "revision": "0cc87f46afb20e4700219d2e9e592028",
+    "url": "/index.html"
+  }
+];
